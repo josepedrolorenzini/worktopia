c6786825f313f585fb136d9d4ba5044d65dcf3b4
josepedrolorenzini <joseplorenzini@gmail.com>
2025-03-23 10:00:30 +1100

    more files

--- helpers.php
+++ helpers.php
@@ -6,8 +6,37 @@
  * @param string $path
  * @return string
  */
-function basePath($path = '') {
-   return rtrim(__DIR__, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR);
+function basePath(string $path = ''): string
+{
+  return rtrim(__DIR__, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . ltrim($path, DIRECTORY_SEPARATOR);
 
   // return __DIR__ . "/" . $path;
 }
+
+/**
+ * Load a view
+ * @param string $name
+ */
+
+function loadView(string $name){
+    require basePath('views/'.$name.'.view.php');
+}
+
+/**
+ * Load partial
+ * @param string $nombre
+ */
+
+function loadPartial(string $nombre = "head"){
+    //var_dump(basePath('views\partials\\' .$nombre .'.php'));
+  //  require basePath('views\\partials\\'.$nombre.'.php');
+    if(basePath('views/partials/'.$nombre.'.php')){
+        return basePath('views/partials/'.$nombre.'.php');
+    }else{
+        error_log( basePath('views/partials/'.$nombre.'.php'));
+        return ;
+    }
+
+}
+
+?>
--- public/index.php
+++ public/index.php
@@ -2,10 +2,16 @@
 
 require __DIR__ . '/../helpers.php'; // Include helper
 
-//var_dump(basePath('views\home.view.php') );
-require basePath('views/home.php') ;
-die;
-use function Worktopia\Helpers\basePath; // Correct import
 
-echo basePath('views\home.php');
+loadView("home")    ;
+die;
+
+
+
+
+//var_dump(loadView('home')) ;
+//require basePath('views\home.view.php') ;
+//use function Worktopia\Helpers\basePath; // Correct import
+//
+//echo basePath('views\home.view.php');
 
